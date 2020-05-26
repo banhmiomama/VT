47671aaf9e7f975aba52de4a43db1caaf1602508
Nguyen Long <thanhlong070899@gmail.com>
2020-05-26 14:13:39 +0700

    Branch

---
+++ VTCinema/Controllers/Branch/BranchController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace VTCinema.Controllers.Branch
+{
+    [Route("Branch")]
+    public class BranchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Branch/BranchView.cshtml");
+        }
+
+        [Route("LoadBranch")]
+        [HttpPost]
+        public string LoadBranch()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+               
+                using (Models.ExecuteDataBase confunc = new Models.ExecuteDataBase())
+                {
+                    dt = confunc.ExecuteDataTable("[YYY_sp_Branch_LoadList]", CommandType.StoredProcedure);
+
+                }
+                if (dt != null)
+                {
+                    return JsonConvert.SerializeObject(dt);
+                }
+                else
+                {
+                    return "[]";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "[]";
+            }
+        }
+    }
+}
---
+++ VTCinema/Controllers/Branch/BranchDetailController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace VTCinema.Controllers.Branch
+{
+    [Route("BranchDetail")]
+    public class BranchDetailController : Controller
+    {
+        [Route("{BranchID}")]
+        [HttpGet]
+        public IActionResult Index(int BranchID)
+        {
+            ViewBag.BranchID = BranchID;
+            return View("~/Views/Branch/BranchDetail.cshtml");
+        }
+        [Route("LoadBranchDetail/{BranchID}")]
+        [HttpPost]
+        public string LoadBranchDetail(int BranchID)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+
+                using (Models.ExecuteDataBase confunc = new Models.ExecuteDataBase())
+                {
+                    dt = confunc.ExecuteDataTable("[YYY_sp_Branch_LoadDetail]", CommandType.StoredProcedure,
+                        "@CurrentID",SqlDbType.Int,BranchID);
+
+                }
+                if (dt != null)
+                {
+                    return JsonConvert.SerializeObject(dt);
+                }
+                else
+                {
+                    return "[]";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "[]";
+            }
+        }
+    }
+
+}
