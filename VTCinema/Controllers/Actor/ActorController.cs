6e6fd99afd9bcbbda88384fded93036eea6a77c1
=BuiTanVuong <=tanvuong18042018@gmail.com>
2020-05-28 10:33:24 +0700

    Actor

--- VTCinema/Controllers/Actor/ActorController.cs
+++ VTCinema/Controllers/Actor/ActorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -41,6 +42,26 @@ namespace VTCinema.Controllers.Actor
             catch (Exception ex)
             {
                 return "[]";
+            }
+        }
+        [Route("DeleteActor/{ID}")]
+        [HttpGet]
+        public string DeleteActor(int ID)
+        {
+            try
+            {
+               DataTable dt = new DataTable();
+                using (Models.ExecuteDataBase confunc = new Models.ExecuteDataBase())
+                {
+                    dt = confunc.ExecuteDataTable("[YYY_sp_Actor_Delete]", CommandType.StoredProcedure,
+                      "@CurrentID", SqlDbType.Int, ID,
+                      "@Modified_By",SqlDbType.Int, HttpContext.Session.GetInt32(Comon.Global.UserID));
+                }
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return "0";
             }
         }
     }
