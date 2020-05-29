bc2b5233e6ad016e32974093fafc6fb323cf3128
Nguyen Long <thanhlong070899@gmail.com>
2020-05-29 09:43:30 +0700

    Branch

---
+++ VTCinema/Models/DataBranchChoose.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VTCinema.Models
+{
+    public class DataBranchChoose
+    {
+        public int? BranchID { get; set; }
+        public string BranchCode { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Address { get; set; }
+        public float? Latitude { get; set; }
+        public float? Longtitude { get; set; }
+        public string Note { get; set; }
+
+    }
+}
