837e0689db4f681a56eeff8043c63988e40f1676
NguyenThanhL0ng <57427498+NguyenThanhL0ng@users.noreply.github.com>
2020-06-01 22:35:30 -0700

    update

--- VTCinema/Controllers/Branch/BranchController.cs
+++ VTCinema/Controllers/Branch/BranchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -42,6 +43,28 @@ namespace VTCinema.Controllers.Branch
             {
                 return "[]";
             }
+        }
+
+        [Route("DeleteBranch/{ID}")]
+        [HttpGet]
+        public string DeleteBranch(int ID)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                using (Models.ExecuteDataBase confunc = new Models.ExecuteDataBase())
+                {
+                    dt = confunc.ExecuteDataTable("[YYY_sp_Branch_Delete]", CommandType.StoredProcedure,
+                      "@CurrentID", SqlDbType.Int, ID,
+                      "@Modified_By", SqlDbType.Int, HttpContext.Session.GetInt32(Comon.Global.UserID));
+                }
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return "0";
+            }
+            LoadBranch();
         }
     }
 }
