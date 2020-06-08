0d166f2a820e8f5b8f3982a941eba6e266b7580e
=BuiTanVuong <=tanvuong18042018@gmail.com>
2020-06-08 18:12:37 +0700

    CRUD Movie Ticket Type & Edit Ages,Sub

---
+++ VTCinema/Models/DataMovieTicketTypeChoose.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VTCinema.Models
+{
+    public class DataMovieTicketTypeChoose
+    {
+        public string Name { get; set; }
+        public int? Price { get; set; }
+        public string Note { get; set; }
+    }
+}
