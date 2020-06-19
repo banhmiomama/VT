0aad057ff65a92a61e96d20036384cfa48df98b6
=BuiTanVuong <=tanvuong18042018@gmail.com>
2020-06-19 15:13:30 +0700

    Main

--- VTCinema/Controllers/Clients/Main/MainController.cs
+++ VTCinema/Controllers/Clients/Main/MainController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace VTCinema.Controllers.Admin.Clients.Main
 {
@@ -12,6 +14,33 @@ namespace VTCinema.Controllers.Admin.Clients.Main
         public IActionResult Index()
         {
             return View("~/Views/Clients/Main/MainView.cshtml");
+        }
+        [Route("LoadDataMain")]
+        [HttpPost]
+        public string LoadDataMain()
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+
+                using (Models.ExecuteDataBase confunc = new Models.ExecuteDataBase())
+                {
+                    ds = confunc.ExecuteDataSet("[YYY_sp_Client_Movie_LoadList_Main]", CommandType.StoredProcedure);
+
+                }
+                if (ds != null)
+                {
+                    return JsonConvert.SerializeObject(ds);
+                }
+                else
+                {
+                    return "[]";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "[]";
+            }
         }
     }
 }
