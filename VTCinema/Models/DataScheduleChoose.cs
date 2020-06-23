3c41afdadb3ecf8345bd2068d8aa1bd2053a1500
=BuiTanVuong <=tanvuong18042018@gmail.com>
2020-06-23 11:46:11 +0700

    CRUD Schedule

---
+++ VTCinema/Models/DataScheduleChoose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VTCinema.Models
+{
+    public class DataScheduleChoose
+    {
+        public int? MovieID { get; set; }
+        public int? BranchID { get; set; }
+        public int? RoomID { get; set; }
+        public int? TicketTypeID { get; set; }      
+        public DateTime ShowTime { get; set; }
+        public DateTime CloseTime { get; set; }
+        public string Note { get; set; }
+    }
+}
